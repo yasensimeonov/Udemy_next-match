4ddf4a8752e4321ff5d0771d73fc28e26106173b
Yasen Simeonov <y.simeonov@cnsys.bg>
2024-12-27 19:19:00 +0200

    Section 3: setup Prisma, generated and applied migration to NpgSQL database.

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,10 @@
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "@/prisma"
-import GitHub from "next-auth/providers/github";
-
+import authConfig from "./auth.config"
+import {prisma} from "@/lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(prisma),
-    providers: [GitHub],
+    session: { strategy: 'jwt' },
+    ...authConfig,
 })
